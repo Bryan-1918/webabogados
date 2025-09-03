cc1401e0d1ec0f4fe2993d7965928ea7bf26e1b0
Bryan Ruiz <bryanrh0704@gmail.com>
2025-09-02 19:30:44 -0500

    se agregó conexión a la base de datos en la clase usuarios.php y código sql

--- php/usuarios.php
+++ php/usuarios.php
@@ -3,6 +3,21 @@ include 'conexion.php';
 $nombreUsuario = $_POST['nombreUsuario'];
 $passUsuario = $_POST['passUsuario'];
 
-echo $nombreUsuario;
-echo $passUsuario;
+//echo $nombreUsuario;
+//echo $passUsuario;
+
+$sql = "SELECT emailUsuario, passUsuario FROM usuarios";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "email: " . $row["emailUsuario"]. " - Password: " . $row["passUsuario"]."<br>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
 ?>

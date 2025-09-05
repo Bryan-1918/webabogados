c578974874d17b558f3663153d6f5fd950310aa6
Bryan Ruiz <bryanrh0704@gmail.com>
2025-09-04 19:35:51 -0500

    se añadió crudUser.html para crear el crud cuando se inicia como superadmin, se modificó usuarios.php para hacer la conexión con la base de datos y verificar los usuarios, se agregó un switch para el Administrador y Usuario

--- php/usuarios.php
+++ php/usuarios.php
@@ -1,19 +1,28 @@
 <?php
 include 'conexion.php';
-$nombreUsuario = $_POST['nombreUsuario'];
+$emailUsuario = $_POST['emailUsuario'];
 $passUsuario = $_POST['passUsuario'];
 
 //echo $nombreUsuario;
 //echo $passUsuario;
 
-$sql = "SELECT emailUsuario, passUsuario FROM usuarios";
+$sql = "SELECT emailUsuario, passUsuario, tipoUsuario FROM usuarios WHERE emailUsuario = '$emailUsuario' AND passUsuario = '$passUsuario'";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
   // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "email: " . $row["emailUsuario"]. " - Password: " . $row["passUsuario"]."<br>";
+  $row = mysqli_fetch_assoc($result);
+  $tipoUsaurio = $row["tipoUsuario"];
+  echo $tipoUsaurio;
+
+  switch($tipoUsaurio) {
+    case "Administrador": echo "Es un Super Usuario";
+                          header('location: ../crudUser.html');
+    break;
+    case "Usuario": echo "Es pepito";
+                    header('location: ../index.html');
   }
+  
 } else {
   echo "0 results";
 }

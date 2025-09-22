dc8d69d7dc009808ec10a392c5bef804ec86c60d
Bryan Ruiz <bryanrh0704@gmail.com>
2025-09-21 22:30:26 -0500

    al registrar un usuario nuevo se pone por defecto 'Usuario' en la base de datos con el fin de que no hayan complicaciones en la base de datos y el campo quede vacío

--- php/registros.php
+++ php/registros.php
@@ -5,15 +5,16 @@ $apellidoUsuario = $_POST['apellido'];
 $emailUsuario = $_POST['email'];
 $telUsuario = $_POST['telefono'];
 $passUsuario = $_POST['password'];
-$tipoUsuario = $_POST['tipoUsuario'];
+$tipoUsuario = 'Usuario';
 
-$sql = "INSERT INTO usuarios (nombreUsuario, apellidoUsuario, emailUsuario, telUsuario, passUsuario, tipoUsuario, created_at, updated_at) VALUES ('$nombreUsuario', '$apellidoUsuario', '$emailUsuario', '$telUsuario', '$passUsuario', '$tipoUsuario', NOW(), NOW())";
+$sql = "INSERT INTO usuarios (nombreUsuario, apellidoUsuario, emailUsuario, telUsuario, passUsuario, tipoUsuario, created_at, updated_at) 
+        VALUES ('$nombreUsuario', '$apellidoUsuario', '$emailUsuario', '$telUsuario', '$passUsuario', '$tipoUsuario', NOW(), NOW())";
 $result = mysqli_query($conn, $sql);
 
 if ($result) {
   echo "<script>
           alert('Usuario creado correctamente ✅');
-          window.location.href = '../login.html'; // <-- Redirige al menú del CRUD
+          window.location.href = '../login.html'; // <-- Redirige al login
         </script>";
 } else {
   if (mysqli_errno($conn) == 1062) { // Código de error de duplicado
